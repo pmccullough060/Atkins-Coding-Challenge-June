388c07e81e6889aaa7a4c72e63c34592c10944e5
Peter McCullough <peter.mccullough@atkinsglobal.com>
2021-06-17 09:03:54 +0100

    first commit

---
+++ src/main.py
@@ -0,0 +1,60 @@
+
+import re
+
+class Row:
+    def __init__(self, phase, forceX, forceY, forceZ):
+        self.phase = phase
+        self.forceX = forceX
+        self.forceY = forceY
+        self.forceZ = forceZ
+
+
+class Table:
+    def __init__(self, name):
+        self.name = name
+        self._rows = []
+
+    def addRow(self, row):
+        self._rows.append(row)
+    
+
+class Results:
+    def __init__(self, fileName):
+        self.fileName = fileName
+        self.tables = []
+
+class Chunker:
+    def __init__(self, startRegexExp, terminateRegexExp, fileName):
+        self.startRegexExp = startRegexExp
+        self.terminateRegexExp = terminateRegexExp
+        self.fileName = fileName
+        self.chunks = []
+
+    def processFile(self):
+        babyChunk = []
+        chunkin = False
+
+        with open(self.fileName) as file:
+            for line in file:
+                if(chunkin):
+                    if(self.finish(line)):
+                        self.chunks.append(babyChunk)
+                        babyChunk.clear()
+                    else:
+                        babyChunk.append(line)
+                else:
+                    chunkin = self.start(line)
+
+    def finish(self, line):
+        return re.search(self.terminateRegexExp, line)
+
+    def start(self, line):
+        return re.search(self.startRegexExp, line) 
+
+    def getChunks(self):
+        return self.chunks
+
+#Test code:
+row = Row(1,2,3,4)
+table = Table("TestTable")
+table.addRow(row)
